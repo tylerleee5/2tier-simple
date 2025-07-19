39d53e78f26ca2f08812a5c6d06b68d164940d1d
tylerlee5 <tyler.li@cybergal.com>
2025-07-18 22:44:17 -0400

    first commit

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+users = {
+    "1": {"name": "Alice"},
+    "2": {"name": "Bob"}
+}
+
+@app.route("/")
+def home():
+    return "Welcome to the 2‑Tier App!"
+
+@app.route("/users/<id>")
+def get_user(id):
+    return jsonify(users.get(id, {"error": "User not found"}))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
